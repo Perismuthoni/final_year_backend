e0598e34b4310df85f7d9baee51dbb8d52f7eee1
perismuthoni <muthonimuriuki22@gmail.com>
2022-03-02 13:04:23 +0300

    routine commit

--- config.py
+++ config.py
@@ -3,12 +3,9 @@ DEBUG = True
 
 # Define the application directory
 import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
-
-# Define the database - we are working with
-# SQLite for this example
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-DATABASE_CONNECT_OPTIONS = {}
+ 
+class Config(object):
+ SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 # Application threads. A common general assumption is
 # using 2 per available processor cores - to handle
